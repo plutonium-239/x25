e801d0a404e4aba78ea4bd44659e6ec396622e7b
Plutonium-239 <plutonium.239.811@gmail.com>
2024-01-18 13:44:56 +0530

    first (and probably last) commit

---
+++ content_sc.js
@@ -0,0 +1,11 @@
+const parent = document.querySelector("#abs-outer > div.extra-services > div.full-text > ul");
+
+const ar5iv_link = location.origin.replace('arxiv', 'ar5iv') + location.pathname;
+
+var ar5iv_element = `
+	<li>
+    	<a href="${ar5iv_link}" class="abs-button">ar5iv HTML</a>
+	</li>`
+
+parent.insertAdjacentHTML('beforeend', ar5iv_element);
+console.log('x25 end');
